ad52f61927169a96cf3858ed70280a318d9d2a57
jbalooshie <jhboshier@gmail.com>
2024-08-16 12:11:27 -0400

    Add function that asks if you want to play again.

--- app.py
+++ app.py
@@ -19,11 +19,27 @@ def gameplay_loop():
         if player_guess == guess_me:
             guesses += 1
             print(f'Congrats! You guessed the number in {guesses} tries.')
+            play_again()
         if player_guess > guess_me:
             guesses += 1
             print('The number I am thinking of is smaller than that. Guess again!')
         if player_guess < guess_me:
             guesses += 1
             print('The number I am thinking of is larger than that. Guess again!')
+
+def play_again():
+    time.sleep(3)
+    choice = ''
+    while choice != 'N':
+        choice = input('Would you like to play again? Y/N')
+        if choice == 'Y':
+            print("Ok! I'm thinking of a new number.")
+            time.sleep(1)
+            gameplay_loop()
+        elif choice == 'N':
+            time.sleep(1)
+            return print('Thanks for playing!')
+        else:
+            print('Invalid selection.')
 
 main_menu()
