2958b9a5f6e1e2b63ce3fa32a3c3410a4f2ebcd2
jbalooshie <jhboshier@gmail.com>
2024-08-16 12:52:48 -0400

    Add main_menu() function to control game, rename previous main_menu() function to greeting().

--- app.py
+++ app.py
@@ -1,45 +1,61 @@
 import random
 import time
 
-def main_menu():
+def greeting():
     print('Welcome to guess the number!')
     time.sleep(1)
     print('I am thinking of a number from 1 to 100.')
     time.sleep(1)
     print('Can you guess it?')
     time.sleep(3)
-    gameplay_loop()
 
 def gameplay_loop():
     guess_me = random.randint(1,100)
     player_guess = 0
     guesses = 0
-    while player_guess != guess_me:
-        player_guess = int(input('Enter your guess.'))
-        if player_guess == guess_me:
-            guesses += 1
-            print(f'Congrats! You guessed the number in {guesses} tries.')
-            play_again()
-        if player_guess > guess_me:
+    while True:
+        try:
+            player_guess = int(input('Enter your guess.'))
+        except ValueError:
+            print('You did not guess a number. Try again.')
+            continue
+        
+        if player_guess > 100 or player_guess < 0:
+            print('That is not a valid guess! The number is from 1 to 100.')
+        elif player_guess > guess_me:
             guesses += 1
             print('The number I am thinking of is smaller than that. Guess again!')
-        if player_guess < guess_me:
+        elif player_guess < guess_me:
             guesses += 1
             print('The number I am thinking of is larger than that. Guess again!')
+        else:
+            guesses += 1
+            print(f'Congrats! You guessed the number in {guesses} tries.')
+            break
+        
 
 def play_again():
     time.sleep(3)
     choice = ''
-    while choice != 'N':
+    while True:
         choice = input('Would you like to play again? Y/N')
         if choice == 'Y':
             print("Ok! I'm thinking of a new number.")
             time.sleep(1)
-            gameplay_loop()
+            return True
         elif choice == 'N':
             time.sleep(1)
-            return print('Thanks for playing!')
+            return False
         else:
             print('Invalid selection.')
 
-main_menu()
+def main_menu():
+    greeting()
+    while True:
+        gameplay_loop()
+        if not play_again():
+            print('Thanks for playing!')
+            break
+
+if __name__ == '__main__':
+    main_menu()
